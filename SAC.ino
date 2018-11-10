2eada42e47fa9ff525a11ba44e61f4c68e158d07
Maitre <guillaume.maitre@student.unamur.be>
2018-11-10 18:58:24 +0100

    V0.1.0 - Multi directional Drunk Driver

--- SAC.ino
+++ SAC.ino
@@ -1,15 +1,22 @@
 //define all the L298N bridge pin that are used to control the 4 motor (interpreted as 2 motor for the board Left/Right)
-#define enA 10
-#define in1 9
-#define in2 8
+//Remake cable to avoid 9-10 of servo blocking the left side motors
+#define enA 2
+#define in1 1
+#define in2 0
 #define enB 5
 #define in3 7
 #define in4 6
+//include the servo library
+#include <Servo.h>
+Servo serv;
+
 
 int rotDirection = 0;
 const int TRIGGER_PIN = A0;
 const int ECHO_PIN = A1;
-const int inter_time = 1000;
+const int inter_time = 500;
+//sensor face foward
+int angle = 153;
 
 void setup() {
   //initialise the sonic sensor
@@ -24,21 +31,33 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   // Set initial rotation direction
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
+  //digitalWrite(in1, LOW);
+  //digitalWrite(in2, HIGH);
+  //digitalWrite(in3, LOW);
+  //digitalWrite(in4, HIGH);
+  //attach the servo to the broche of the car and put it to the face
+  serv.attach(9);
+  serv.write(angle);
+}
+
+
+//return distance from sensor
+int getDistance(){
+  unsigned long duration;
+  float distance;
+  digitalWrite(TRIGGER_PIN, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(TRIGGER_PIN, LOW);
+  duration = pulseIn (ECHO_PIN, HIGH);
+  distance = (duration / 2.0) / 29.0;
+  delay(inter_time);
+  return distance;
 }
 
 void loop() {
   //Get information from the sonic range sensor
-  unsigned long duration;
-  float distance;
-  digitalWrite(TRIGGER_PIN, HIGH);
-  delayMicroseconds(500);
-  digitalWrite(TRIGGER_PIN, LOW);
-  duration = pulseIn (ECHO_PIN, HIGH);
-  distance = (duration / 2.0) / 29.0;
+  float distance, dist1, dist2;
+  distance = getDistance();
   Serial.print("Data:");
   Serial.print (millis() / 1000.0);
   Serial.print(", d = ");
@@ -47,25 +66,58 @@ void loop() {
   delay(inter_time);
 
 
-  //If no object at less than 15 centimeter continue to drive
-  if (distance > 15) {
+  //If no object at less than 20 centimeter continue to drive
+  if (distance > 20) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
     digitalWrite(in3, LOW);
     digitalWrite(in4, HIGH);
-    Serial.print(pwmOutput);
     analogWrite(enA, 150);
     analogWrite(enB, 150);
+    rotDirection = 0;
+    Serial.println(rotDirection);
   }
 
-  //Stop motor and invert the motor direction turn left
-  if (distance <= 15) {
-    int i = 255;
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-    analogWrite(enA, 100);
-    analogWrite(enB, 100);
+  //Stop motor and invert the motor direction to turn left
+  if (distance <= 20) {
+    //analogWrite(enA, 0);
+    //analogWrite(enB, 0);
+    
+
+    //check on left and right to choose the better place to go
+    angle = 179;
+    serv.write(angle);
+    dist1 = getDistance();
+    delayMicroseconds(1000);
+    angle = 127;
+    serv.write(angle);
+    dist2 = getDistance();
+    delayMicroseconds(1000);
+    angle = 153;
+    serv.write(angle);
+
+    if (dist1 >= dist2){
+      //turn left
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      analogWrite(enA, 100);
+      analogWrite(enB, 100);
+      rotDirection = 1;
+      Serial.println(rotDirection);
+      delayMicroseconds(1000);
+    } else {
+      //turn right
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      analogWrite(enA, 100);
+      analogWrite(enB, 100);
+      rotDirection = 2;
+      Serial.println(rotDirection);
+      delayMicroseconds(1000);
+    }
   }
 }
